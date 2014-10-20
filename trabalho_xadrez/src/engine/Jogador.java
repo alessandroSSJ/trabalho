fffee3c8ffb8ad0624da515dfb444fba114fb46d
Alessandro <alessandro.ssj@gmail.com>
2014-10-20 16:21:43 -0200

    new class

---
+++ trabalho_xadrez/src/engine/Jogador.java
@@ -0,0 +1,16 @@
+/**
+ * Um objeto do tipo jogador é responsável por gravar e documentar as informações
+ * referentes à um jogador do xadrez.
+ * 
+ * @version 1.0
+ * @author Alessandro e Marcelo
+ * @since 1.0
+ */
+
+package engine;
+
+public class Jogador {
+	
+	/* Lado do jogador */
+	private static int lado;
+}
